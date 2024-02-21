b0cc350efdcd2f3684441ad760cee7b6aa864bac
vugonz <goncalooazevedo@outlook.com>
2024-02-21 11:56:14 +0000

    fix: add debug mode to client CLI arguments and format code
    format code with google-java-format

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
@@ -3,6 +3,12 @@ package pt.ulisboa.tecnico.tuplespaces.client;
 import pt.ulisboa.tecnico.tuplespaces.client.grpc.ClientService;
 
 public class ClientMain {
+    public static boolean DEBUG_MODE = false;
+
+    public static void debug(String s) {
+        System.err.println("[DEBUG]: " + s);
+    }
+
     public static void main(String[] args) {
 
         System.out.println(ClientMain.class.getSimpleName());
@@ -14,9 +20,9 @@ public class ClientMain {
         }
 
         // check arguments
-        if (args.length != 3) {
+        if (args.length < 3) {
             System.err.println("Argument(s) missing!");
-            System.err.println("Usage: mvn exec:java -Dexec.args=<host> <port>");
+            System.err.println("Usage: mvn exec:java -Dexec.args=<host> <port> <service_name> [--debug]");
             return;
         }
 
@@ -24,8 +30,13 @@ public class ClientMain {
         final String host = args[0];
         final String port = args[1];
 
+        if (args.length == 4) {
+            if (args[3].equals("--debug")) {
+                DEBUG_MODE = true;
+            }
+        }
+
         CommandProcessor parser = new CommandProcessor(new ClientService());
         parser.parseInput();
-
     }
 }

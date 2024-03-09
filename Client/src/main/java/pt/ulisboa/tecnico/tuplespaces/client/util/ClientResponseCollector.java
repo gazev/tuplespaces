9fa84bfefd346b0335f7d1207013ee5b750849e0
João Tiago <joao.leal.tintas@tecnico.ulisboa.pt>
2024-03-09 11:43:38 +0000

    feat: implement ClientResponseCollector class

---
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/util/ClientResponseCollector.java
@@ -0,0 +1,44 @@
+package pt.ulisboa.tecnico.tuplespaces.client.util;
+
+import static pt.ulisboa.tecnico.tuplespaces.client.ClientMain.debug;
+
+import java.util.List;
+
+public class ClientResponseCollector {
+    List<String> responses;
+    List<Exception> exceptions;
+
+    public ClientResponseCollector() {}
+
+    public ClientResponseCollector(List<String> responses) {
+        this.responses = responses;
+    }
+
+    public synchronized void saveException(Exception e) {
+        debug(String.format("Call ClientResponseCollector::saveException: message=%s", e.getMessage()));
+        exceptions.add(e);
+        notifyAll();
+    }
+
+    public synchronized void saveResponse(String response) {
+        debug(String.format("Call ClientResponseCollector::saveResponse: response=%s", response));
+        responses.add(response);
+        notifyAll();
+    }
+
+    public synchronized List<String> getResponses() {
+        debug("Call ClientResponseCollector::getResponses");
+        return responses;
+    }
+
+    public synchronized void waitAllResponses(int n) {
+        debug(String.format("Call ClientResponseCollector::waitAllResponses: responseNumber=%d", n));
+        while ( (responses.size() + exceptions.size()) < n) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
